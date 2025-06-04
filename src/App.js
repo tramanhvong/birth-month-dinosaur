d6c44743bf5927a8b01e922a1c0d825e01a873ed
Ngoc Tram Anh Vong <vongta21@my.yorku.ca>
2025-06-04 13:29:10 -0400

    finished. deploying

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useState } from 'react';
+import ReactDOM from 'react-dom/client';
 
 function App() {
   const [month, setMonth] = useState("january");
+
   const renderDinosaur = (month) => {
     month = month.toLowerCase();
-    <div className="dinosaur">
-            <img src={`../public/photo_stocks/${month}.jpg`} className="dinosaurImg" alt="logo" />
-    </div>
+    const container = document.getElementsByClassName("dinosaurPlaceholder")[0];
+    const root = ReactDOM.createRoot(container)
+    root.render(
+      <div className="dinosaur">
+        <img src={`/photos/${month}.jpg`} className="dinosaurImg" alt="dinosaur" />
+      </div>
+    );
   }
   return (
-    <div className="App">
+    <main className="App">
       <header className="header">
         <h1>Which Dinosaur are you according to your birth month</h1>
         <h2>Credits to @crankbookscience on Instagram</h2>
       </header>
 
-      <body className="body">
+      <div className="body">
         <div className="selectMonth">
-          <label htmlFor="month">Select your birth month:</label>
-          <select id="month" name="month" value={month} onChange={(e) => setMonth(e.target.value)}>            <option value="january">January</option>
+          <label htmlFor="month" className="question">Select your birth month: </label>
+          <select className="selector" id="month" name="month" value={month} onChange={(e) => setMonth(e.target.value)}>            
+            <option value="january">January</option>
             <option value="february">February</option>
             <option value="march">March</option>
             <option value="april">April</option>
@@ -33,10 +40,11 @@ function App() {
             <option value="november">November</option>
             <option value="december">December</option>
           </select>
-          <button type="button" onClick={() => renderDinosaur(month)}>Get Dinosaur</button>
         </div>
-      </body>
-    </div>
+        <button type="button" className="submitButton" onClick={() => renderDinosaur(month)}>Get Dinosaur</button>
+        <div className="dinosaurPlaceholder"></div>
+      </div>
+    </main>
   );
 }
 
